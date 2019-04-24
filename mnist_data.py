a234ef507fe60a25e1d40fea94b052461d003ad9
Seulki Lee <seulki@cs.unc.edu>
2019-04-24 08:02:39 -0400

    update

--- mnist_data.py
+++ mnist_data.py
@@ -1,6 +1,3 @@
-import struct
-import numpy as np
-
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
@@ -12,48 +9,3 @@ def validation_set():
 	
 def test_set():
 	return mnist.test.images, mnist.test.labels
-
-def write_data():
-	f = open('mnist_test', 'w')
-	print mnist.test.images.shape
-	data_len = mnist.test.images.shape[0]
-	single_data_size = mnist.test.images.shape[1]
-	print 'data_len', data_len
-	print 'single_data_size', single_data_size
-
-	f.write(struct.pack('i', data_len))
-	f.write(struct.pack('i', single_data_size))
-
-	for image in mnist.test.images:
-		#print len(image)
-		for i in range(len(image)):
-			f.write(struct.pack('f', image[i]))
-
-	f.close()
-
-def write_label():
-	mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
-	f = open('mnist_label', 'w')
-	data_len = mnist.test.labels.shape[0]
-	#single_data_size = mnist.test.labels.shape[1]
-	single_data_size = 1
-	print 'data_len', data_len
-	print 'single_data_size', single_data_size
-
-	f.write(struct.pack('i', data_len))
-	f.write(struct.pack('i', single_data_size))
-
-	for label in mnist.test.labels:
-		f.write(struct.pack('b', label))
-	f.close()
-
-
-def main():
-	"""
-	write_data()
-	write_label()
-	"""
-	return
-
-if __name__ == '__main__':
-	main()
